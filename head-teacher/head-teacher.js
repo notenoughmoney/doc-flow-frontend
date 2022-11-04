b9c1bbd6ff6621103c6e06f6d32d2204b9d34bf4
Дмитрий <kr.dmitry@mail.ru>
2022-11-04 15:34:15 +0800

    Добавил обработку ошибок при входе

--- head-teacher/head-teacher.js
+++ head-teacher/head-teacher.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+
+  // ставим имя работника
+  $('#fio').text(localStorage.getItem('fullName'));
+  $('#job').text(localStorage.getItem('job'));
+
   $('#example').DataTable( {
       language: {
           url: 'https://cdn.datatables.net/plug-ins/1.12.1/i18n/ru.json'
---
+++ signin/signin.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    const url="https://localhost:7263/api/LogIn";
+    $('#login').click(function(){
+
+        // проверяем на правильность mail
+        if (!$('#mail').val().includes('@')) {
+            $('#mailEx').modal('show');
+            return;
+        }
+        
+        const data = {
+            mail: $('#mail').val(),
+            password: $('#password').val()
+        }
+
+        $.ajax({
+            contentType: 'application/json',
+            type: "POST",
+            url: url,
+            data: JSON.stringify(data),
+            dataType: "json",
+            success: function(result) {
+                if (result.length === 0) {
+                    $('#enterEx').modal('show');
+                } else {
+                    console.log(result[0]);
+                    // устанавиливаем local storage
+                    localStorage.setItem("fullName", result[0].fullName);
+                    localStorage.setItem("job", result[0].job);
+                    if (result[0].jobId >= 3 && result[0].jobId <= 5) {
+                        window.location.href = '..\\head-teacher\\head-teacher.html';
+                    }
+                }
+            },
+            error: function(error){
+                console.log(error);
+            }
+        })
+    })
+})
